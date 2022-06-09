ddb603a7a4aee8c86618e202f9ceb28267ae1d35
looklsh <looklsh38@gmail.com>
2022-06-09 17:26:42 +0900

    dotenet6-study

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Get: /HelloWorld
+        //public string Index()
+        //{
+        //    return "This is my default action.";
+        //}
+
+        // Get: /HelloWorld/Welcome
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+
+        //public string Welcome(string name, int id = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {id}");
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["Numtimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
